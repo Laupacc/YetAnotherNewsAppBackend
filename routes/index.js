182e6bef691bba788078ae75ce68500d987464c1
Your name <Your email>
2024-05-05 23:38:07 +0200

    d

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 router.get('/search', (req, res) => {
-    const search = req.body.search;
+    const search = req.query.q;
     fetch(`https://newsdata.io/api/1/news?apikey=${NEWS_API_KEY}&q=${search}&image=1&language=en`)
         .then(response => response.json())
         .then(data => {
@@ -15,7 +15,12 @@ router.get('/search', (req, res) => {
             } else {
                 res.json({ articles: [] });
             }
+        })
+        .catch(error => {
+            console.error("Error fetching news:", error);
+            res.status(500).json({ error: "Error fetching news" });
         });
 });
+
 
 module.exports = router;
