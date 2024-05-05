04b5485e8ae594c10904d736782851f0442e24b2
Your name <Your email>
2024-05-05 13:16:59 +0200

    more

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,8 @@ const fetch = require('node-fetch');
 
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
-
 // articles from the verge
-router.get('/articles', (req, res) => {
+router.get('/theverge', (req, res) => {
   fetch(`https://newsapi.org/v2/top-headlines?sources=the-verge&apiKey=${NEWS_API_KEY}`)
     .then(response => response.json())
     .then(data => {
@@ -19,6 +18,7 @@ router.get('/articles', (req, res) => {
     });
 });
 
+// articles from Canada
 router.get('/canada', (req, res) => {
   fetch(`https://newsapi.org/v2/top-headlines?country=ca&apiKey=${NEWS_API_KEY}`)
     .then(response => response.json())
