ec5d6edb0bae4be07221292d5ca411f0e9be81c3
Your name <Your email>
2024-05-06 13:07:45 +0200

    q

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 router.get('/search', (req, res) => {
     const search = req.query.q;
-    fetch(`https://newsdata.io/api/1/news?apikey=${NEWS_API_KEY}&q=${search}&image=1&language=en`)
+    fetch(`https://newsdata.io/api/1/news?apikey=${NEWS_API_KEY}&qInMeta=${search}&image=1&language=en`)
         .then(response => response.json())
         .then(data => {
             if (data.status === 'success') {
