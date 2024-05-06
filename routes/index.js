9959a66e22b72cf48f431c3970a4c055d1adf755
Your name <Your email>
2024-05-06 13:08:30 +0200

    q

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 router.get('/search', (req, res) => {
-    const search = req.query.q;
+    const search = req.query.qInMeta;
     fetch(`https://newsdata.io/api/1/news?apikey=${NEWS_API_KEY}&qInMeta=${search}&image=1&language=en`)
         .then(response => response.json())
         .then(data => {
